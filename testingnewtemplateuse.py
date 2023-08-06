ded5ac79f686f50ad137ecb11a929ef589456ca4
Britt767 <byer777@outlook.com>
2023-08-06 14:17:02 -0700

    Changed 8/6.

---
+++ testingnewtemplateuse.py
@@ -0,0 +1,6 @@
+# _______________________________________________________ #
+# Title: Test Template
+# Description: Testing new template
+# ChangeLog: 8/6/23
+# _______________________________________________________ #
+print("Hi, how are you?")
