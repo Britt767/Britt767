fc4215d7ea00a3bd2419a0a80eac026414ad48ee
Britt767 <byer777@outlook.com>
2023-08-06 14:09:17 -0700

    Changed 8/6.

---
+++ ClassTemplate.py
@@ -0,0 +1,6 @@
+# __________________________________________________ #
+# Title:  NewTesting_GitHub
+# Description: Like the title says
+# ChangeLog: 8/6/23
+# __________________________________________________ #
+print("Hello Brian, you are getting there!")
