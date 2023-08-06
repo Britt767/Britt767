4637c5f037c21ac282ef817e8c4f60ff0a77580f
Britt767 <byer777@outlook.com>
2023-08-06 14:24:39 -0700

    Changed 8/6.

---
+++ Try_Except.py
@@ -0,0 +1,12 @@
+# _______________________________________________________ #
+# Title: try_except
+# Description: Try / except in python
+# ChangeLog: 8/6/23
+# _______________________________________________________ #
+try:
+    fltN1 = 5.0
+    fltN2 = 0.0
+    fltQuotient = fltN1 / fltN2
+except:
+
+    print("An error occurred when trying to divide", fltN1, "by", fltN2)
